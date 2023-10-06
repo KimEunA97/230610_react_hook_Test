a33c9e02a5426c55c8561e85ddc15d9ebba46931
KimEunA97 <kimeuna9712@gmail.com>
2023-10-06 18:44:10 +0900

    useCallback으로 리렌더링 없이 함수 재사용

--- whattodo/src/components/TodoInsert.js
+++ whattodo/src/components/TodoInsert.js
@@ -1,10 +1,21 @@
 import { MdAdd } from 'react-icons/md'
 import './TodoInsert.scss'
+import { useCallback, useState } from 'react'
 
 const TodoInsert = () => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback(e => {
+    setValue(e.target.value);
+  }, []);
+
   return (
     <form className='TodoInsert'>
-      <input placeholder='할 일 입력' />
+      <input
+        placeholder='할 일 입력'
+        value={value}
+        onChange={onChange}
+      />
       <button type='submit'>
         <MdAdd />
       </button>
